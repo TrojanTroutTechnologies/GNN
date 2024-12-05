16d7c347be1cc6e845b8031ad5d2d7b034c1da9d
Payton Webber <paytonwebber@gmail.com>
2024-12-04 18:16:48 -0800

    refactor: removed wandb sweep code

--- src/train.py
+++ src/train.py
@@ -1,11 +1,9 @@
 import os
-import argparse
 import numpy as np
 import torch
 import torch.nn as nn
 import torch_geometric as pyg
 from tqdm import tqdm
-import wandb
 
 from utils import load_npz, to_graph, get_metadata, rollout
 from gnn_network import LearnedSimulator
@@ -13,19 +11,6 @@ from gnn_network import LearnedSimulator
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 if torch.backends.mps.is_available():
     device = torch.device("mps")
-
-sweep_configuration = {
-    "name": "gnn_sweep",
-    "method": "bayes",
-    "metric": {"goal": "minimize", "name": "avg_loss"},
-    "parameters": {
-        "lr": {"min": 0.0001, "max": 0.1},
-        "batch_size": {"values": [4, 8, 16]},
-        "optimizer": {"values": ["adam", "sgd"]},
-        "noise_std": {"min": 0.0001, "max": 0.001},
-        "window_size": {"min": 3, "max": 9},
-    },
-}
 
 
 class OneStepDataset(pyg.data.Dataset):
@@ -77,22 +62,16 @@ class OneStepDataset(pyg.data.Dataset):
 
 def rolloutMSE(
     simulator: torch.nn.Module,
-    valid_simulation: pyg.loader.DataLoader,
+    rollout_data: pyg.data.Dataset,
     metadata: dict,
-    # noise:
 ):
-    total_loss = 0.0
-    batch_count = 0
     simulator.eval()
-
     with torch.no_grad():
-        for window in valid_simulation:
-            rollout_out = rollout(simulator, window, metadata)
-            rollout_out = rollout_out.permute(1, 0, 2)
-            loss = ((rollout_out - window["position"]) ** 2).sum(dim=-1).mean()
-            total_loss += loss.item()
-            batch_count += 1
-    return total_loss / batch_count
+        rollout_traj = rollout(simulator, rollout_data, metadata)
+        ground_truth = torch.from_numpy(
+            np.array(rollout_data.data[0].tolist()["simulation_0"][0])
+        )
+        return ((rollout_traj - ground_truth) ** 2).mean()
 
 
 def oneStepMSE(
@@ -125,131 +104,12 @@ def oneStepMSE(
     return total_loss / batch_count, total_mse / batch_count
 
 
-def sweep_train(
-    params,
-    simulator: torch.nn.Module,
-    train_loader: pyg.data.DataLoader,
-    valid_loader: pyg.data.DataLoader,
-    metadata: dict,
-    epochs: int = 1,
-    load_epoch: int = 0,
-    sweep: bool = False,
-) -> None:
-
-    loss_fn = nn.MSELoss()
-    optimizer = torch.optim.Adam(simulator.parameters(), lr=params.lr)
-
-    scheduler = torch.optim.lr_scheduler.ExponentialLR(
-        optimizer, gamma=0.1 ** (1 / 5e6)
-    )
-
-    # log the loss
-    total_step = 0
-
-    for i in range(epochs):
-        simulator.train()
-        total_loss = 0
-        batch_count = 0
-
-        # Only train on the first 1000 batches
-        progress_bar = tqdm(train_loader, desc=f"Epoch {i}")
-        for batch in progress_bar:
-            if total_step > 1000:
-                break
-            optimizer.zero_grad()
-
-            batch = batch.to(device)
-            preds = simulator(batch)
-
-            loss = loss_fn(preds, batch.y)
-            loss.backward()
-
-            optimizer.step()
-            scheduler.step()
-            total_loss += loss.item()
-            batch_count += 1
-            progress_bar.set_postfix(
-                {
-                    "loss": loss.item(),
-                    "avg_loss": total_loss / batch_count,
-                    "lr": optimizer.param_groups[0]["lr"],
-                }
-            )
-
-            if sweep:
-                wandb.log(
-                    {
-                        "avg_loss": total_loss / batch_count,
-                        "lr": optimizer.param_groups[0]["lr"],
-                    }
-                )
-
-            total_step += 1
-
-            if total_step % 1000 == 0:
-                simulator.eval()
-                eval_loss, onestep_mse = oneStepMSE(simulator, valid_loader, metadata)
-                tqdm.write(f"\nEval: Loss: {eval_loss}, One Step MSE: {onestep_mse}")
-                simulator.train()
-
-    return
-
-
-def start_train_sweep():
-    wandb.init(project="gnn_project")
-    params = wandb.config
-
-    metadata = get_metadata()
-
-    train_dataset = OneStepDataset(
-        "data/processed/train.npz",
-        metadata,
-        window_size=params.window_size,
-        noise_std=params.noise_std,
-    )
-    valid_dataset = OneStepDataset(
-        "data/processed/valid.npz",
-        metadata,
-        window_size=params.window_size,
-        noise_std=0.0,
-    )
-    train_loader = pyg.loader.DataLoader(
-        train_dataset,
-        batch_size=params.batch_size,
-        shuffle=True,
-        pin_memory=True,
-        num_workers=2,
-    )
-    valid_loader = pyg.loader.DataLoader(
-        valid_dataset,
-        batch_size=params.batch_size,
-        shuffle=False,
-        pin_memory=True,
-        num_workers=2,
-    )
-
-    print(f"Using device: {device}")
-
-    simulator = LearnedSimulator(window_size=params.window_size)
-    simulator = simulator.to(device)
-
-    sweep_train(
-        params,
-        simulator,
-        train_loader,
-        valid_loader,
-        metadata,
-        epochs=1,
-        load_epoch=0,
-        sweep=True,
-    )
-
-
-def train(
+def train_loop(
     params: dict,
     simulator: torch.nn.Module,
     train_loader: pyg.data.DataLoader,
     valid_loader: pyg.data.DataLoader,
+    rollout_data: pyg.data.Dataset,
     metadata: dict,
 ) -> None:
     loss_fn = nn.MSELoss()
@@ -297,7 +157,7 @@ def train(
 
             if total_step % params["rollout_interval"] == 0:
                 simulator.eval()
-                rollout_loss = rolloutMSE(simulator, valid_loader, metadata)
+                rollout_loss = rolloutMSE(simulator, rollout_data, metadata)
                 tqdm.write(f"\nRollout Loss: {rollout_loss}")
                 simulator.train()
 
@@ -330,7 +190,7 @@ def train(
     return
 
 
-def default_train():
+def start_training():
     params = {
         "epochs": 5,
         "batch_size": 8,
@@ -391,15 +251,8 @@ def default_train():
         )
         simulator.load_state_dict(checkpoint["model"])
 
-    train(params, simulator, train_loader, valid_loader, metadata)
+    train_loop(params, simulator, train_loader, valid_loader, valid_dataset, metadata)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--sweep", type=bool, default=False)
-    args = parser.parse_args()
-    if args.sweep:
-        sweep_id = wandb.sweep(sweep_configuration, project="second_sweep")
-        wandb.agent(sweep_id, function=start_train_sweep)
-    else:
-        default_train()
+    start_training()
